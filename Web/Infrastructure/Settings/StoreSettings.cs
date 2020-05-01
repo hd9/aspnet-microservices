b9c29a7817a849806b0ab538b3bc91b6c236dd00
Bruno Hildenbrand <hd9x@outlook.com>
2020-05-01 12:45:06 -0600

    Web: added settings for price, site name and site url.

--- Web/Infrastructure/Settings/StoreSettings.cs
+++ Web/Infrastructure/Settings/StoreSettings.cs
@@ -10,6 +10,9 @@ namespace Web.Infrastructure.Settings
         public string Country { get; set; }
         public List<string> Regions { get; set; }
         public string Currency { get; set; }
+        public string CurrencyDisplay { get; set; }
+        public string StoreName { get; set; }
+        public string StoreUrl { get; set; }
         public decimal Tax { get; set; }
     }
 }
